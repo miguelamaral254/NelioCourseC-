30164744e56c2a3bdddb97447efe3aa438af4837
Miguel Amaral <miguel.amaral.sales@gmail.com>
2024-01-06 16:56:19 -0300

    at

--- CotacaoDolar/Conversor.cs
+++ CotacaoDolar/Conversor.cs
@@ -2,15 +2,16 @@ namespace CotacaoDolar
 {
 
 
-class Conversor
-{
+    class Conversor
+    {
 
-   public static double Iof = 6.0;
-    
- public static double  Result(double CotValue, double BuyDol) {
-    double total = CotValue * BuyDol;
-    return total + total * (Iof / 100.0);
-}
-}
+        public static double Iof = 6.0;
+
+        public static double Result(double CotValue, double BuyDol)
+        {
+            double total = CotValue * BuyDol;
+            return total + total * (Iof / 100.0);
+        }
+    }
 
 }
--- CotacaoDolar/Program.cs
+++ CotacaoDolar/Program.cs
@@ -1,23 +1,28 @@
-﻿namespace CotacaoDolar
+﻿using System;
+using System.Globalization;
+namespace CotacaoDolar
+
 {
 
-class Program
-{
-static void Main(string[] args)
+    class Program
     {
-    
+        static void Main(string[] args)
+        {
 
 
 
-    
-    double CotValue = double.Parse(Console.ReadLine());
-    double BuyDol = double.Parse(Console.ReadLine());
 
-    double Resultado = Conversor.Result(BuyDol, CotValue);
+            Console.WriteLine("Valor da cotação: ");
+            double CotValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Valor da compra: ");
+            double BuyDol = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-    Console.WriteLine($"{Resultado}");
-        
-     
+            double Resultado = Conversor.Result(BuyDol, CotValue);
+            Console.WriteLine("---------");
+            
+            Console.WriteLine("Valor a ser pago em R$ = " + Resultado.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
     }
-}
 }
