6816fa1027f5dbfcc84207aba984c0bb7975bd00
Miguel <miguel.amaral.sales@gmail.com>
2024-02-22 22:30:38 -0300

    Sec 10

---
+++ Heranca/Entities/Account.cs
@@ -0,0 +1,24 @@
+﻿namespace Heranca.Entities;
+internal class Account
+{
+    public int Number { get; set; }
+    public string Holder { get; set; }
+    public double Balance { get; protected set; }
+
+    public Account() { }
+
+    public Account(int number, string holder, double balance)
+    {
+        Number = number;
+        Holder = holder;
+        Balance = balance;
+    }
+    public void WithDraw(double amount)
+    {
+        Balance -= amount;
+    }
+    public void Deposit(double amount)
+    {
+        Balance += amount;
+    }
+}
